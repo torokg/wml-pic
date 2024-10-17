ceb7e14d0bca1b12ae142b6f32bbce5221f628b3
Géza Török <geza.torok@neuroforge.org>
2024-10-17 22:43:54 +0200

    proximity sensor

--- WaterMinilabIO.X/drivers/VNCL4040/VNCL4040.cc
+++ WaterMinilabIO.X/drivers/VNCL4040/VNCL4040.cc
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <io/host/service.hh>
 #include "../VNCL4040.h"
 
 bool
@@ -10,22 +11,31 @@ VNCL4040::init()
     // Turn off ALS & disable ALS interrupt in ALS_CONF register
     {
         uint8_t cmd[] = {0x0,0b00000001,0b00000000};
-        if(!i2c.Write(address, (uint8_t*)&cmd, 3));
+        if(!i2c.Write(address, cmd, 3))
+        {
+            io::host::log("Failed to disable ALS mode in proximity sensor");
             return false;
+        }
     }
 
-    // Turn on & setup PS in PS_CONF1 register
+    // Turn on PS in PS_CONF1 register
     {
         uint8_t cmd[] = {0x03,0b00000000,0b00001000};
-        if(!i2c.Write(address, (uint8_t*)&cmd, 3));
+        if(!i2c.Write(address, cmd, 3))
+        {
+            io::host::log("Failed to turn on PS mode in proximity sensor");
             return false;
+        }
     }
 
-    // Turn on & setup PS in PS_CONF1 register
+    // Setup PS in PS_CONF3 register
     {
         uint8_t cmd[] = {0x04,0b01110001,0b00000111};
-        if(!i2c.Write(address, (uint8_t*)&cmd, 3));
+        if(!i2c.Write(address, cmd, 3))
+        {
+            io::host::log("Failed to configure PS mode in proximity sensor");
             return false;
+        }
     }
 
     initialized = true;
@@ -42,7 +52,8 @@ bool
 VNCL4040::read(uint16_t &value)
 {   
     std::unique_lock ul(lk);
-    init();
+    if(!init())
+        return false;
     uint8_t cmd = 0x08;
     if(!i2c.WriteRead(address, &cmd, 1, (uint8_t*)&value, 2))
     {
